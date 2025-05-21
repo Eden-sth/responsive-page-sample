d2084a0ad8324ef864dfd42a664bd28f5f95504a
Eden-sth <cseo99737@gmail.com>
2025-05-21 18:06:41 +0900

    자바스크립트 업데이트

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// 헤더 애니메이션 추가
+
+const header = document.querySelector(".header h1");
+
+const keyframes = {
+  opacity: [0, 1],
+  translate: [`0 100px`, 0],
+};
+
+const options = {
+  duration: 500,
+  easing: "ease-in",
+  //   direction: "normal",
+  //   iteration: Infinity,
+};
+
+header.animate(keyframes, options);
