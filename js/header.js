ab428495418b927aa57451ca4b9afdae83c0f2a4
Eden-sth <cseo99737@gmail.com>
2025-05-22 14:20:05 +0900

    상단배너 업뎃

--- js/header.js
+++ js/header.js
@@ -3,12 +3,12 @@
 const header = document.querySelector(".header h1");
 
 const keyframes = {
-  opacity: [0, 1],
+  // opacity: [0, 1],
   translate: [`0 100px`, 0],
 };
 
 const options = {
-  duration: 500,
+  duration: 1000,
   easing: "ease-in",
   //   direction: "normal",
   //   iteration: Infinity,
