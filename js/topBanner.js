f4276f912134df60ba9fd46744c24ce7809fa7f1
Eden-sth <cseo99737@gmail.com>
2025-05-27 11:38:05 +0900

    파일 재업로드

--- js/topBanner.js
+++ js/topBanner.js
@@ -1,14 +1,23 @@
-const topBanner = document.querySelector(".topBanner ul");
+const carousel = document.querySelector(".carousel");
 
-const keyframes = {
-  transform: ["translateX(0px)", "translateX(500px)"],
+const carouselKeyframes = {
+  transform: ["translateX(0px)", "translateX(-100px)"],
 };
-
-const options = {
-  duration: 1000,
-  easing: "ease-in",
-  direction: `normal`,
+const carouselOption = {
+  duration: 15000,
   iterations: Infinity,
 };
 
-topBanner.animate(keyframes, options);
+let animations = [];
+
+carousel.forEach((carousel) => {
+  let animation = carousel.animate(carouselKeyframes, carouselOption);
+  animations.push(animation);
+});
+
+carouselWrap.addEventLister("mouseover", () => {
+  animations.forEach((animation) => animation.pause());
+});
+carouselWrap.addEventLister("mouseout", () => {
+  animations.forEach((animation) => animation.play());
+});
